8e4acbd20a162f239ddd550e00199e61bd67e354
Anil <anil.gudur@clariontechnologies.co.in>
2016-09-27 16:49:27 +0530

    step-6 'Two-way Data Binding' added

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -9,14 +9,18 @@ angular.
 		this.phones = [
 		    {
 			name: 'Nexus S',
-			snippet: 'Fast just got faster with Nexus S.'
+			snippet: 'Fast just got faster with Nexus S.',
+			age: 1
 		    }, {
-			name: 'Motorola XOOM™ with Wi-Fi',
-			snippet: 'The Next, Next Generation tablet.'
+			name: 'Motorola XOOM\u2122 with Wi-Fi',
+			snippet: 'The Next, Next Generation tablet.',
+			age: 2
 		    }, {
-			name: 'MOTOROLA XOOM™',
-			snippet: 'The Next, Next Generation tablet.'
+			name: 'MOTOROLA XOOM\u2122',
+			snippet: 'The Next, Next Generation tablet.',
+			age: 3
 		    }
 		];
+		this.orderProp = 'age';
 	    }
 	});
